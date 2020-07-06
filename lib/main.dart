18e6a9857c90bcc76426d5c06f7889025b4dc9c3
MustafaCEVIK <cevikksoft@gmail.com>
2020-07-06 11:12:56 +0300

    Generate login screen design.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:solidarity_flutter_ui/screens/main_tabbar.dart';
+import 'package:solidarity_flutter_ui/screens/login/login.dart';
+//import 'package:solidarity_flutter_ui/screens/main_tabbar.dart';
 
 void main() => runApp(SolidarityApp());
 
@@ -7,40 +8,39 @@ class SolidarityApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Solidarity App',
-        home: MainTabBar(),
-        theme: ThemeData(
-            // colors
-            brightness: Brightness.light,
-            primaryColor: Color(0xff12389b),
-            accentColor: Color(0xff1818D5),
-            // font family
-            fontFamily:
-                'Montserrat', // TODO : research best font family & colors
-            // text themes
-            textTheme: TextTheme(
-                headline1: TextStyle(
-                    letterSpacing: 1,
-                    fontSize: 22,
-                    fontWeight: FontWeight.w800,
-                    color: Colors.black),
-                headline6:
-                    TextStyle(fontSize: 8.0, fontStyle: FontStyle.italic),
-                bodyText1:
-                    TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
-                bodyText2: TextStyle(
-                    fontSize: 14.0, color: Colors.grey, fontFamily: 'Hind'),
-                overline: TextStyle(
-                    fontSize: 13.0,
-                    fontWeight: FontWeight.w800,
-                    color: Color(0xff1818D5))),
-            // appbar theme
-            appBarTheme:
-                AppBarTheme(color: Theme.of(context).scaffoldBackgroundColor),
-            // tabbar theme
-            tabBarTheme: TabBarTheme(
-              labelColor: Color(0xff12389b),
-              unselectedLabelColor: Colors.grey,
-            )));
+      title: 'Solidarity App',
+      home: LoginScreen(),
+      theme: ThemeData(
+        // colors
+        brightness: Brightness.light,
+        primaryColor: Color(0xff12389b),
+        accentColor: Color(0xff1818D5),
+        // font family
+        fontFamily: 'Montserrat', // TODO : research best font family & colors
+        // text themes
+        textTheme: TextTheme(
+            headline1: TextStyle(
+                letterSpacing: 1,
+                fontSize: 22,
+                fontWeight: FontWeight.w800,
+                color: Colors.black),
+            headline6: TextStyle(fontSize: 8.0, fontStyle: FontStyle.italic),
+            bodyText1: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+            bodyText2: TextStyle(
+                fontSize: 14.0, color: Colors.grey, fontFamily: 'Hind'),
+            overline: TextStyle(
+                fontSize: 13.0,
+                fontWeight: FontWeight.w800,
+                color: Color(0xff1818D5))),
+        // appbar theme
+        appBarTheme:
+            AppBarTheme(color: Theme.of(context).scaffoldBackgroundColor),
+        // tabbar theme
+        tabBarTheme: TabBarTheme(
+          labelColor: Color(0xff12389b),
+          unselectedLabelColor: Colors.grey,
+        ),
+      ),
+    );
   }
 }
---
+++ lib/screens/login/login.dart
@@ -0,0 +1,225 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  Widget _buildEmailTextField() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          'Email',
+          style: kLabelStyle,
+        ),
+        SizedBox(height: 10.0),
+        Container(
+          alignment: Alignment.centerLeft,
+          decoration: kBoxDecorationStyle,
+          height: 60.0,
+          child: TextField(
+            keyboardType: TextInputType.emailAddress,
+            style: TextStyle(
+              color: Colors.white,
+              fontFamily: 'OpenSans',
+            ),
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              contentPadding: EdgeInsets.only(top: 14.0),
+              prefixIcon: Icon(
+                Icons.email,
+                color: Colors.white,
+              ),
+              hintText: 'Enter your email',
+              hintStyle: kHintTextStyle,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildPasswordTextField() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          'Password',
+          style: kLabelStyle,
+        ),
+        SizedBox(height: 10.0),
+        Container(
+          alignment: Alignment.centerLeft,
+          decoration: kBoxDecorationStyle,
+          height: 60.0,
+          child: TextField(
+            obscureText: true,
+            style: TextStyle(
+              color: Colors.white,
+              fontFamily: 'OpenSans',
+            ),
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              contentPadding: EdgeInsets.only(top: 14.0),
+              prefixIcon: Icon(
+                Icons.lock,
+                color: Colors.white,
+              ),
+              hintText: 'Enter your password',
+              hintStyle: kHintTextStyle,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildLoginButton() {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 25.0),
+      width: double.infinity,
+      child: RaisedButton(
+        elevation: 5.0,
+        onPressed: () => print('Login Button Pressed'),
+        padding: EdgeInsets.all(15.0),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30.0),
+        ),
+        color: Colors.white,
+        child: Text(
+          'LOGIN',
+          style: TextStyle(
+            color: Color(0xFF527DAA),
+            letterSpacing: 1.5,
+            fontSize: 18.0,
+            fontWeight: FontWeight.bold,
+            fontFamily: 'OpenSans',
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildSignupButton() {
+    return GestureDetector(
+      onTap: () => print('Sign Up Button Pressed'),
+      child: RichText(
+        text: TextSpan(
+          children: [
+            TextSpan(
+              text: 'Don\'t have an account? ',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 16.0,
+                fontWeight: FontWeight.w400,
+                fontFamily: 'OpenSans',
+              ),
+            ),
+            TextSpan(
+              text: 'Sign Up',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 16.0,
+                fontWeight: FontWeight.bold,
+                fontFamily: 'OpenSans',
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: AnnotatedRegion<SystemUiOverlayStyle>(
+        value: SystemUiOverlayStyle.light,
+        child: GestureDetector(
+          onTap: () => FocusScope.of(context).unfocus(),
+          child: Stack(
+            children: <Widget>[
+              Container(
+                height: double.infinity,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [
+                      Color(0xFF73AEF5),
+                      Color(0xFF61A4F1),
+                      Color(0xFF478DE0),
+                      Color(0xFF398AE5),
+                    ],
+                    stops: [0.1, 0.4, 0.7, 0.8],
+                  ),
+                ),
+              ),
+              Container(
+                height: double.infinity,
+                child: SingleChildScrollView(
+                  physics: AlwaysScrollableScrollPhysics(),
+                  padding: EdgeInsets.only(
+                    top: 120.0,
+                    bottom: 20.0,
+                    left: 30.0,
+                    right: 30.0,
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        'Sign In',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontFamily: 'OpenSans',
+                          fontSize: 30.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: 30.0),
+                      _buildEmailTextField(),
+                      SizedBox(height: 30.0),
+                      _buildPasswordTextField(),
+                      SizedBox(height: 20.0),
+                      _buildLoginButton(),
+                      SizedBox(height: 90.0),
+                      _buildSignupButton()
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+final kHintTextStyle = TextStyle(
+  color: Colors.white54,
+  fontFamily: 'OpenSans',
+);
+
+final kLabelStyle = TextStyle(
+  color: Colors.white,
+  fontWeight: FontWeight.bold,
+  fontFamily: 'OpenSans',
+);
+
+final kBoxDecorationStyle = BoxDecoration(
+  color: Color(0xFF6CA8F1),
+  borderRadius: BorderRadius.circular(10.0),
+  boxShadow: [
+    BoxShadow(
+      color: Colors.black12,
+      blurRadius: 6.0,
+      offset: Offset(0, 2),
+    ),
+  ],
+);
