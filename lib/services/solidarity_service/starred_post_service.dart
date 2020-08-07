2de96196ac7593b528932ecf9752c6e3933ff8d4
MustafaCEVIK <cevikksoft@gmail.com>
2020-08-07 14:58:02 +0300

    Generate starred_post_service_test & fix problem starred_post_service functions.

--- lib/services/solidarity_service/starred_post_service.dart
+++ lib/services/solidarity_service/starred_post_service.dart
@@ -8,7 +8,7 @@ import 'package:solidarity_flutter_ui/models/starred_post_model.dart';
 import 'package:solidarity_flutter_ui/models/user_model.dart';
 import 'package:solidarity_flutter_ui/utils/shared_prefs.dart';
 
-final _apiUrl = "http://localhost:2020/starredposts";
+final _apiUrl = "http://solidarity-backend.herokuapp.com/starredposts";
 
 Future<List<dynamic>> getStarredPostMyPosts() async {
   final response = await http.get("$_apiUrl/getmystarredposts",
@@ -44,7 +44,7 @@ Future<List<Post>> getStarredPostsByUserId(String userId) async {
 }
 
 Future<List<User>> getStarredPostUsersByPostId(String postId) async {
-  final response = await http.get("$_apiUrl/getbyuserid/$postId",
+  final response = await http.get("$_apiUrl/getbypostid/$postId",
       headers: {"token": SharedPrefs.getToken});
 
   if (response.statusCode == 200) {
@@ -57,7 +57,10 @@ Future<List<User>> getStarredPostUsersByPostId(String postId) async {
 Future<bool> addStarredPost(AddStarredPostDTO addStarredPostDTO) async {
   final response = await http.post(
     "$_apiUrl/add",
-    headers: {HttpHeaders.contentTypeHeader: "application/json"},
+    headers: {
+      HttpHeaders.contentTypeHeader: "application/json",
+      "token": SharedPrefs.getToken,
+    },
     body: addStarredPostToJson(addStarredPostDTO),
   );
 
@@ -71,7 +74,10 @@ Future<bool> addStarredPost(AddStarredPostDTO addStarredPostDTO) async {
 Future<bool> deleleStarredPost(String deleteStarredPostId) async {
   final response = await http.delete(
     "$_apiUrl/delete/$deleteStarredPostId",
-    headers: {HttpHeaders.contentTypeHeader: "application/json"},
+    headers: {
+      HttpHeaders.contentTypeHeader: "application/json",
+      "token": SharedPrefs.getToken,
+    },
   );
 
   if (response.statusCode == 200) {
---
+++ test/services/solidarity_service/starred_post_service_test.dart
@@ -0,0 +1,57 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:solidarity_flutter_ui/models/dtos/add_starred_post_dto.dart';
+import 'package:solidarity_flutter_ui/models/dtos/login_dto.dart';
+import 'package:solidarity_flutter_ui/services/solidarity_service/auth_service.dart';
+import 'package:solidarity_flutter_ui/services/solidarity_service/starred_post_service.dart';
+import 'package:solidarity_flutter_ui/utils/shared_prefs.dart';
+
+void main() async {
+  SharedPreferences.setMockInitialValues({});
+  await SharedPrefs.init();
+
+  final _login =
+      LoginDTO(email: "semustafacevik@gmail.com", password: "c123123");
+  await login(_login);
+
+  final _starredPostId = "5f2d38629c97020017493bd0";
+  final _postId = "5ef789fc398c473790a4e728";
+  final _userId = "5ef7803367077a26d8b0f2db";
+
+  final _newPostId = "5f19aa46d8bff6125c33b829";
+  final _addStarredPostDTO = AddStarredPostDTO(
+    postId: _newPostId,
+  );
+  group("Solidarity Service - Starred Post Service Test Functions", () {
+    test("POST: addStarredPost", () async {
+      var result = await addStarredPost(_addStarredPostDTO);
+      expect(result, true);
+    });
+
+    test("GET: getStarredPostMyPosts", () async {
+      var result = await getStarredPostMyPosts();
+      expect(result[0].runtimeType, String);
+    });
+
+    test("GET: getStarredPostById", () async {
+      var result = await getStarredPostById(_starredPostId);
+      expect(result.user.id, _userId);
+      expect(result.post.id, _postId);
+    });
+
+    test("GET: getStarredPostsByUserId", () async {
+      var result = await getStarredPostsByUserId(_userId);
+      expect(result[0].userId, _userId);
+    });
+
+    test("GET: getStarredPostUsersByPostId", () async {
+      var result = await getStarredPostUsersByPostId(_postId);
+      expect(result[0].id, _userId);
+    });
+
+    test("DEL: deleleStarredPost", () async {
+      var result = await deleleStarredPost(_newPostId);
+      expect(result, true);
+    });
+  });
+}
