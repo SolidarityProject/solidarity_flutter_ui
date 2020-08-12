4c9a48618e4af8a7b81dccca15461fd6245dd814
MustafaCEVIK <cevikksoft@gmail.com>
2020-08-12 03:00:25 +0300

    Generate account screen & route.

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,6 +1,9 @@
 class Constants {
-  static const String ROUTE_LOGIN = "/login";
-  static const String ROUTE_TABCONTROLLER = "/tabcontroller";
+  static const String ROUTE_ACCOUNT = "/account";
   static const String ROUTE_HOME = "/home";
+  static const String ROUTE_LOGIN = "/login";
   static const String ROUTE_SEARCH = "/search";
+  static const String ROUTE_SPLASH = "/splash";
+  static const String ROUTE_STARRED = "/starred";
+  static const String ROUTE_TABCONTROLLER = "/tabcontroller";
 }
--- lib/utils/routes.dart
+++ lib/utils/routes.dart
@@ -1,15 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:solidarity_flutter_ui/screens/account_screen.dart';
 import 'package:solidarity_flutter_ui/screens/home_screen.dart';
 import 'package:solidarity_flutter_ui/screens/login_screen.dart';
+import 'package:solidarity_flutter_ui/screens/splash_screen.dart';
+import 'package:solidarity_flutter_ui/screens/starred_screen.dart';
 import 'package:solidarity_flutter_ui/screens/tab_controller_screen.dart';
 import 'package:solidarity_flutter_ui/screens/search_screen.dart';
 import 'package:solidarity_flutter_ui/utils/constants.dart';
 
 class Routes {
   static final routes = <String, WidgetBuilder>{
-    Constants.ROUTE_LOGIN: (BuildContext context) => LoginScreen(),
-    Constants.ROUTE_TABCONTROLLER: (BuildContext context) => TabControllerScreen(),
+    Constants.ROUTE_ACCOUNT: (BuildContext context) => AccountScreen(),
     Constants.ROUTE_HOME: (BuildContext context) => HomeScreen(),
+    Constants.ROUTE_LOGIN: (BuildContext context) => LoginScreen(),
     Constants.ROUTE_SEARCH: (BuildContext context) => SearchScreen(),
+    Constants.ROUTE_SPLASH: (BuildContext context) => SplashScreen(),
+    Constants.ROUTE_STARRED: (BuildContext context) => StarredScreen(),
+    Constants.ROUTE_TABCONTROLLER: (BuildContext context) =>
+        TabControllerScreen(),
   };
 }
