ebfbf11f52c855b196a0aa97ba7b2a043f39c78f
MustafaCEVIK <cevikksoft@gmail.com>
2020-08-19 14:44:13 +0300

    Done profile screen validations.

--- lib/mixins/validation_mixin/profile_validation_mixin.dart
+++ lib/mixins/validation_mixin/profile_validation_mixin.dart
@@ -1,34 +1,69 @@
 class ProfileValidationMixin {
   String validateName(String value) {
-    if (value.isEmpty) {
+    if (value.startsWith(" ")) {
+      return "Name mustn't start space";
+    } else if (value.contains("  ")) {
+      return "Name mustn't be contains one more than space";
+    } else if (value.isEmpty) {
       return "Please enter your name";
     } else if (value.length < 2) {
       return "Name must be at least 2 characters";
+    } else {
+      return null;
     }
-    return null;
   }
 
   String validateLastName(String value) {
-    if (value.isEmpty) {
+    if (value.startsWith(" ")) {
+      return "Last name mustn't start space";
+    } else if (value.contains("  ")) {
+      return "Name mustn't be contains one more than space";
+    } else if (value.isEmpty) {
       return "Please enter your last name";
+    } else if (value.length < 2) {
+      return "Last name must be at least 2 characters";
+    } else {
+      return null;
     }
-
-    return null;
   }
 
   String validateUsername(String value) {
     if (value.isEmpty) {
       return "Please enter your username";
+    } else if (value.contains(" ")) {
+      return "Username mustn't be contains space";
+    } else if (value.length < 4) {
+      return "Username must be at least 4 characters";
+    } else if (!_usernameValidControl(value)) {
+      return "Please check your username format";
+    } else {
+      return null;
     }
-
-    return null;
   }
 
   String validateEmail(String value) {
     if (value.isEmpty) {
       return "Please enter your email";
+    } else if (value.contains(" ")) {
+      return "Email mustn't be contains space";
+    } else if (!_emailValidControl(value)) {
+      return "Please check your email format";
+    } else {
+      return null;
     }
-
-    return null;
   }
+}
+
+bool _usernameValidControl(String username) {
+  Pattern usernamePattern = r"^(?!.*\.\.)(?!.*\.$)[^\W][\w.]{0,29}$";
+  bool usernameValid = RegExp(usernamePattern).hasMatch(username);
+
+  return usernameValid;
+}
+
+bool _emailValidControl(String email) {
+  Pattern emailPattern = r"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$";
+  bool emailValid = RegExp(emailPattern).hasMatch(email);
+
+  return emailValid;
 }
