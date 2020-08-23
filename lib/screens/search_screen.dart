dbe09aaef2040c8aea33fb4ac3353b99b1acf6de
MustafaCEVIK <cevikksoft@gmail.com>
2020-08-24 00:46:46 +0300

    Generate district drop down button.

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:solidarity_flutter_ui/models/country_model.dart';
+import 'package:solidarity_flutter_ui/models/district_model.dart';
 import 'package:solidarity_flutter_ui/models/province_model.dart';
 import 'package:solidarity_flutter_ui/screens/tab_controller_screen.dart';
+import 'package:solidarity_flutter_ui/services/address_service/district_service.dart';
 import 'package:solidarity_flutter_ui/services/address_service/province_service.dart';
 import 'package:solidarity_flutter_ui/utils/styles.dart';
 
@@ -16,12 +18,17 @@ Country selectedCountry;
 List<Province> provinces;
 Province selectedProvince;
 
+List<District> districts;
+District selectedDistrict;
+
 String _provinceHintText;
+String _districtHintText;
 
 class _SearchScreenState extends State<SearchScreen> {
   @override
   void initState() {
     _provinceHintText = "Please select country first";
+    _districtHintText = "Please select country & province first";
     super.initState();
   }
 
@@ -30,6 +37,7 @@ class _SearchScreenState extends State<SearchScreen> {
     return Column(children: [
       _futureBuilderCountryList(),
       _dropDownProvince(),
+      _dropDownDistrict(),
     ]);
   }
 
@@ -76,9 +84,13 @@ class _SearchScreenState extends State<SearchScreen> {
                     setState(() {
                       selectedCountry = value;
                       _provinceHintText = "loading...";
+                      _districtHintText = "Please select province first";
                     });
                     provinces = null;
                     selectedProvince = null;
+                    districts = null;
+                    selectedDistrict = null;
+
                     var newProvinces =
                         await getProvincesByCountryId(selectedCountry.id);
                     setState(() {
@@ -120,15 +132,66 @@ class _SearchScreenState extends State<SearchScreen> {
                       ),
                     ),
                     value: selectedProvince,
-                    onChanged: (value) {
+                    onChanged: (value) async {
                       setState(() {
                         selectedProvince = value;
+                        _districtHintText = "loading...";
+                      });
+                      districts = null;
+                      selectedDistrict = null;
+
+                      var newDistricts =
+                          await getDistrictsByProvinceId(selectedProvince.id);
+
+                      setState(() {
+                        districts = newDistricts;
+                        _districtHintText = "Please select district";
                       });
                     },
                     items: provinces != null
                         ? provinces.map((province) {
                             return DropdownMenuItem<Province>(
                                 value: province, child: Text(province.name));
+                          }).toList()
+                        : null),
+              ),
+            ),
+          ],
+        ),
+      );
+
+  Widget _dropDownDistrict() => Padding(
+        padding: EdgeInsets.fromLTRB(20, 20, 20, 5),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text("District", style: Styles.TF_LABEL),
+            SizedBox(height: 10.0),
+            Container(
+              padding: EdgeInsets.only(left: 15, right: 10),
+              decoration: Styles.TF_BOXDEC.copyWith(
+                border: Border.all(color: Theme.of(context).accentColor),
+              ),
+              child: DropdownButtonHideUnderline(
+                child: DropdownButton<District>(
+                    isExpanded: true,
+                    hint: Text(
+                      _districtHintText,
+                      style: Styles.TF_HINT.copyWith(
+                        fontStyle: FontStyle.italic,
+                      ),
+                    ),
+                    value: selectedDistrict,
+                    onChanged: (value) {
+                      setState(() {
+                        selectedDistrict = value;
+                      });
+                    },
+                    items: districts != null
+                        ? districts.map((district) {
+                            return DropdownMenuItem<District>(
+                                value: district, child: Text(district.name));
                           }).toList()
                         : null),
               ),
