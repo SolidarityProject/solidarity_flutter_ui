14fec38b01eeda1cd104fe8c5aa5a2e2daefb2db
MustafaCEVIK <cevikksoft@gmail.com>
2020-08-24 16:31:45 +0300

    Generate change password screen & add route information.

--- lib/utils/routes.dart
+++ lib/utils/routes.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:solidarity_flutter_ui/screens/account_screen.dart';
+import 'package:solidarity_flutter_ui/screens/change_password_screen.dart';
 import 'package:solidarity_flutter_ui/screens/home_screen.dart';
 import 'package:solidarity_flutter_ui/screens/login_screen.dart';
 import 'package:solidarity_flutter_ui/screens/profile_screen.dart';
@@ -12,6 +13,8 @@ import 'package:solidarity_flutter_ui/utils/constants.dart';
 class Routes {
   static final routes = <String, WidgetBuilder>{
     Constants.ROUTE_ACCOUNT: (BuildContext context) => AccountScreen(),
+    Constants.ROUTE_CHANGEPASSWORD: (BuildContext context) =>
+        ChangePasswordScreen(),
     Constants.ROUTE_HOME: (BuildContext context) => HomeScreen(),
     Constants.ROUTE_LOGIN: (BuildContext context) => LoginScreen(),
     Constants.ROUTE_PROFILE: (BuildContext context) => ProfileScreen(),
