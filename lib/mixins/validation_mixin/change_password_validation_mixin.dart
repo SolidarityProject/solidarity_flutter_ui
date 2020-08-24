e4dfd9701bdcd55410bb8f425624d3d2ba293746
MustafaCEVIK <cevikksoft@gmail.com>
2020-08-24 20:07:38 +0300

    Add new controls -> new password & new password again.

--- lib/mixins/validation_mixin/change_password_validation_mixin.dart
+++ lib/mixins/validation_mixin/change_password_validation_mixin.dart
@@ -9,16 +9,17 @@ class ChangePasswordValidationMixin {
     }
   }
 
-  // void saveName(String newValue) {
-  //   user.name = newValue;
-  // }
+  String _tempPassword;
 
   String validateNewPassword(String value) {
     if (value.isEmpty) {
       return "Please enter your new password";
     } else if (value.length < 6) {
       return "Password must be at least 6 characters";
+    } else if (!_passwordValidControl(value)) {
+      return "Password must contain at least one lowercase letter,\none uppercase letter, one number and one special\ncharacter.";
     } else {
+      _tempPassword = value;
       return null;
     }
   }
@@ -28,8 +29,20 @@ class ChangePasswordValidationMixin {
       return "Please enter your new password again";
     } else if (value.length < 6) {
       return "Password must be at least 6 characters";
+    } else if (!_passwordValidControl(value)) {
+      return "Password must contain at least one lowercase letter,\none uppercase letter, one number and one special\ncharacter.";
+    } else if (_tempPassword != value) {
+      return "New password and repeat password don't match.";
     } else {
       return null;
     }
+  }
+
+  bool _passwordValidControl(String password) {
+    Pattern passwordPattern =
+        r"^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z])(?=.*[$&+,:;=?@#|<>.^\_*()%!-]).{6,15}$";
+    bool passwordValid = RegExp(passwordPattern).hasMatch(password);
+
+    return passwordValid;
   }
 }
