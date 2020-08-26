392739fb31c97779fbe044d412c6265b7e1f9de4
MustafaCEVIK <cevikksoft@gmail.com>
2020-08-26 21:55:25 +0300

    Refactoring change_password_validation_mixin & profile_validation_mixin class.

--- lib/mixins/validation_mixin/change_password_validation_mixin.dart
+++ lib/mixins/validation_mixin/change_password_validation_mixin.dart
@@ -1,48 +1,15 @@
+import 'package:solidarity_flutter_ui/utils/validation_rules.dart';
+
 class ChangePasswordValidationMixin {
   String validateOldPassword(String value) {
-    if (value.isEmpty) {
-      return "Please enter your password";
-    } else if (value.length < 6) {
-      return "Password must be at least 6 characters";
-    } else {
-      return null;
-    }
+    return ValidationRules.validateOldPassword(value);
   }
 
-  String _tempPassword;
-
   String validateNewPassword(String value) {
-    if (value.isEmpty) {
-      return "Please enter your new password";
-    } else if (value.length < 6) {
-      return "Password must be at least 6 characters";
-    } else if (!_passwordValidControl(value)) {
-      return "Password must contain at least one lowercase letter,\none uppercase letter, one number and one special\ncharacter.";
-    } else {
-      _tempPassword = value;
-      return null;
-    }
+    return ValidationRules.validateNewPassword(value);
   }
 
   String validateNewPasswordAgain(String value) {
-    if (value.isEmpty) {
-      return "Please enter your new password again";
-    } else if (value.length < 6) {
-      return "Password must be at least 6 characters";
-    } else if (!_passwordValidControl(value)) {
-      return "Password must contain at least one lowercase letter,\none uppercase letter, one number and one special\ncharacter.";
-    } else if (_tempPassword != value) {
-      return "New password and repeat password don't match.";
-    } else {
-      return null;
-    }
-  }
-
-  bool _passwordValidControl(String password) {
-    Pattern passwordPattern =
-        r"^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z])(?=.*[$&+,:;=?@#|<>.^\_*()%!-]).{6,15}$";
-    bool passwordValid = RegExp(passwordPattern).hasMatch(password);
-
-    return passwordValid;
+    return ValidationRules.validateNewPasswordAgain(value);
   }
 }
--- lib/mixins/validation_mixin/profile_validation_mixin.dart
+++ lib/mixins/validation_mixin/profile_validation_mixin.dart
@@ -1,87 +1,38 @@
 import 'package:solidarity_flutter_ui/screens/tab_controller_screen.dart';
+import 'package:solidarity_flutter_ui/utils/validation_rules.dart';
 
 class ProfileValidationMixin {
   String validateName(String value) {
-    if (value.startsWith(" ")) {
-      return "Name mustn't start space";
-    } else if (value.contains("  ")) {
-      return "Name mustn't be contains one more than space";
-    } else if (value.isEmpty) {
-      return "Please enter your name";
-    } else if (value.length < 2) {
-      return "Name must be at least 2 characters";
-    } else {
-      return null;
-    }
-  }
-
-  void saveName(String newValue) {
-    user.name = newValue;
+    return ValidationRules.validateName(value);
   }
 
   String validateLastName(String value) {
-    if (value.startsWith(" ")) {
-      return "Last name mustn't start space";
-    } else if (value.contains("  ")) {
-      return "Name mustn't be contains one more than space";
-    } else if (value.isEmpty) {
-      return "Please enter your last name";
-    } else if (value.length < 2) {
-      return "Last name must be at least 2 characters";
-    } else {
-      return null;
-    }
+    return ValidationRules.validateLastName(value);
+  }
+
+  String validateUsername(String value) {
+    return ValidationRules.validateUsername(value);
+  }
+
+  String validateEmail(String value) {
+    return ValidationRules.validateEmail(value);
+  }
+
+  //* on saved functions
+
+  void saveName(String newValue) {
+    user.name = newValue;
   }
 
   void saveLastName(String newValue) {
     user.lastname = newValue;
   }
 
-  String validateUsername(String value) {
-    if (value.isEmpty) {
-      return "Please enter your username";
-    } else if (value.contains(" ")) {
-      return "Username mustn't be contains space";
-    } else if (value.length < 4) {
-      return "Username must be at least 4 characters";
-    } else if (!_usernameValidControl(value)) {
-      return "Please check your username format";
-    } else {
-      return null;
-    }
-  }
-
   void saveUsername(String newValue) {
     user.username = newValue;
-  }
-
-  String validateEmail(String value) {
-    if (value.isEmpty) {
-      return "Please enter your email";
-    } else if (value.contains(" ")) {
-      return "Email mustn't be contains space";
-    } else if (!_emailValidControl(value)) {
-      return "Please check your email format";
-    } else {
-      return null;
-    }
   }
 
   void saveEmail(String newValue) {
     user.email = newValue;
   }
-}
-
-bool _usernameValidControl(String username) {
-  Pattern usernamePattern = r"^(?!.*\.\.)(?!.*\.$)[^\W][\w.]{0,29}$";
-  bool usernameValid = RegExp(usernamePattern).hasMatch(username);
-
-  return usernameValid;
-}
-
-bool _emailValidControl(String email) {
-  Pattern emailPattern = r"^[\w-\.]+@([\w-]+\.)+[\w-]{2,3}$";
-  bool emailValid = RegExp(emailPattern).hasMatch(email);
-
-  return emailValid;
 }
