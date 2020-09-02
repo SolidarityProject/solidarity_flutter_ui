3e9cb8a5ff79a4a1d0a0008d10cabc280e121478
MustafaCEVIK <cevikksoft@gmail.com>
2020-09-02 15:50:14 +0300

    Fix post detail navigation function -> home_screen.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -102,7 +102,7 @@ class _HomeScreenState extends State<HomeScreen> {
             context,
             Constants.ROUTE_POSTDETAIL,
             arguments: currentPost.id,
-          );
+          ).then((_) => setState(() {}));
         },
       );
 
