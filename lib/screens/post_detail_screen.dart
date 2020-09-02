8994418b80b7bf06c5d7b016078a7d79859a37f0
MustafaCEVIK <cevikksoft@gmail.com>
2020-09-02 03:30:14 +0300

    Start post_detail_screen design (add image container, add main column).

--- lib/screens/post_detail_screen.dart
+++ lib/screens/post_detail_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:solidarity_flutter_ui/models/post_model.dart';
 import 'package:solidarity_flutter_ui/utils/styles.dart';
 
@@ -7,21 +8,89 @@ class PostDetailScreen extends StatefulWidget {
   _PostDetailScreenState createState() => _PostDetailScreenState();
 }
 
+Post _post;
+
 class _PostDetailScreenState extends State<PostDetailScreen> {
   @override
   Widget build(BuildContext context) {
-    final Post post = ModalRoute.of(context).settings.arguments;
+    _post = ModalRoute.of(context).settings.arguments;
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          "Post Detail",
-          style: Styles.BLACK_TEXT,
-        ),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text(post.title),
+      appBar: _buildAppBar(),
+      body: _buildMainContainer(),
+    );
+  }
+
+  AppBar _buildAppBar() {
+    return AppBar(
+      centerTitle: true,
+      title: Text(
+        "Post Detail",
+        style: Styles.BLACK_TEXT,
       ),
     );
   }
+
+  Container _buildMainContainer() {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      child: SingleChildScrollView(
+        physics: AlwaysScrollableScrollPhysics(),
+        child: _buildMainColumn(),
+      ),
+    );
+  }
+
+  Widget _buildMainColumn() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        _buildImageContainer(),
+        Container(
+          padding: EdgeInsets.symmetric(vertical: 20, horizontal: 30),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                _localDateFormat(
+                  "tr_TR",
+                  _post.dateSolidarity.toLocal(),
+                ),
+                style: Styles.POST_DATE,
+              ),
+              SizedBox(height: 8),
+              Text(
+                _post.title,
+                style: Styles.POST_TITLE,
+              ),
+              SizedBox(height: 8),
+              Text(
+                _post.description,
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildImageContainer() {
+    return Container(
+      height: 250,
+      width: double.infinity,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: NetworkImage(_post.pictureUrl),
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+
+  String _localDateFormat(String locale, DateTime date) =>
+      DateFormat.yMMMMEEEEd(locale).format(date) +
+      " - " +
+      DateFormat.Hm(locale).format(date);
 }
