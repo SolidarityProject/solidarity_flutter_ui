014e7c7070d9163844e1a6df74a367879ac7f81e
MustafaCEVIK <cevikksoft@gmail.com>
2020-09-03 15:02:46 +0300

    Add _appBarTitleSelection (change app bar title when change tab) & refactoring this screen.

--- lib/screens/tab_controller_screen.dart
+++ lib/screens/tab_controller_screen.dart
@@ -12,6 +12,7 @@ import 'package:solidarity_flutter_ui/services/solidarity_service/post_service.d
 import 'package:solidarity_flutter_ui/services/solidarity_service/starred_service.dart';
 import 'package:solidarity_flutter_ui/utils/constants.dart';
 import 'package:solidarity_flutter_ui/utils/shared_prefs.dart';
+import 'package:solidarity_flutter_ui/utils/styles.dart';
 
 class TabControllerScreen extends StatefulWidget {
   @override
@@ -27,6 +28,8 @@ User user;
 Address searchAddress;
 
 class _TabControllerScreenState extends State<TabControllerScreen> {
+  int _currentTabIndex = 0;
+
   @override
   void initState() {
     token = SharedPrefs.getToken;
@@ -51,57 +54,92 @@ class _TabControllerScreenState extends State<TabControllerScreen> {
     );
   }
 
-  Widget _appBar() => AppBar(
-        elevation: 2,
-        centerTitle: true,
-        title: SizedBox(
+  Widget _appBar() {
+    return AppBar(
+      elevation: 2,
+      centerTitle: true,
+      leading: _appBarLeading(),
+      title: _appBarTitleSelection(),
+      actions: <Widget>[
+        Container(
+          height: 40,
+          width: 40,
+          child: InkWell(
+              onTap: () {
+                // TODO : district or city diaolog
+              },
+              child: Icon(Icons.settings)),
+        )
+      ],
+    );
+  }
+
+  Widget _appBarLeading() {
+    return Padding(
+      padding: const EdgeInsets.all(7.0),
+      child: InkWell(
+        onTap: () async => await Navigator.pushNamed(
+          context,
+          Constants.ROUTE_PROFILE,
+        ),
+        child: CircleAvatar(
+          backgroundImage: NetworkImage(user.pictureUrl),
+          backgroundColor: Theme.of(context).primaryColor,
+        ),
+      ),
+    );
+  }
+
+  Widget _appBarTitleSelection() {
+    Widget _titleWidget;
+    switch (_currentTabIndex) {
+      case 0:
+        _titleWidget = SizedBox(
           height: 40,
           width: 40,
           child: Image.asset("assets/images/icon_mobile.png"),
-        ),
-        leading: Padding(
-          padding: const EdgeInsets.all(7.0),
-          child: InkWell(
-            onTap: () async => await Navigator.pushNamed(
-              context,
-              Constants.ROUTE_PROFILE,
-            ),
-            child: CircleAvatar(
-              backgroundImage: NetworkImage(user.pictureUrl),
-              backgroundColor: Theme.of(context).primaryColor,
-            ),
-          ),
-        ),
-        actions: <Widget>[
-          Container(
-            height: 40,
-            width: 40,
-            child: InkWell(
-                onTap: () {
-                  // TODO : district or city diaolog
-                },
-                child: Icon(Icons.settings)),
-          )
-        ],
-      );
+        );
+        break;
 
-  Widget _tabBarView() => TabBarView(
-        physics: NeverScrollableScrollPhysics(),
-        children: <Widget>[
-          HomeScreen(),
-          SearchScreen(),
-          StarredScreen(),
-          AccountScreen()
-        ],
-      );
+      case 1:
+        _titleWidget = Text("Search", style: Styles.BLACK_TEXT);
+        break;
 
-  Widget _tabBarItems() => TabBar(
-        indicator: BoxDecoration(),
-        tabs: <Widget>[
-          Tab(icon: Icon(Icons.home, size: 28.0)),
-          Tab(icon: Icon(Icons.search, size: 28.0)),
-          Tab(icon: Icon(Icons.star, size: 28.0)),
-          Tab(icon: Icon(Icons.dehaze, size: 28.0)),
-        ],
-      );
+      case 2:
+        _titleWidget = Text("Starred", style: Styles.BLACK_TEXT);
+        break;
+
+      case 3:
+        _titleWidget = Text("Account", style: Styles.BLACK_TEXT);
+        break;
+    }
+    return _titleWidget;
+  }
+
+  Widget _tabBarView() {
+    return TabBarView(
+      physics: NeverScrollableScrollPhysics(),
+      children: <Widget>[
+        HomeScreen(),
+        SearchScreen(),
+        StarredScreen(),
+        AccountScreen()
+      ],
+    );
+  }
+
+  Widget _tabBarItems() {
+    return TabBar(
+      onTap: (value) => setState(() {
+        _currentTabIndex = value;
+      }),
+      indicator: BoxDecoration(),
+      tabs: <Widget>[
+        Tab(icon: Icon(Icons.home, size: 28.0)),
+        Tab(icon: Icon(Icons.search, size: 28.0)),
+        Tab(icon: Icon(Icons.star, size: 28.0)),
+        Tab(icon: Icon(Icons.dehaze, size: 28.0)),
+      ],
+    );
+  }
 }
